44e6d3b95b6fa00ca8099976a2f95fbbadf1e552
Razqa <razqaazaki55@gmail.com>
2026-04-02 17:26:01 +0700

    Modul 5

--- Latihan/Karyawan.java
+++ Latihan/Karyawan.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 public class Karyawan {
     private String idKaryawan;
     private String nama;
+    private double gajiPokok; //soal no.2
+    private String departemen; //soal no. 3 
 
+    //Soal no. 1 
     //method constructor
     public Karyawan(String idKaryawan, String nama){
         this.idKaryawan = idKaryawan;
@@ -25,6 +28,34 @@ public class Karyawan {
     public void setNama(String nama) {
         this.nama = nama;
     }
+    //getter gaji pokok
+    public double getGajiPokok(){
+        return this.gajiPokok;
+    }
+    //soal no. 2 getter gaji pokok
+    public void getGajiPokok(double gajiPokok){
+        this.gajiPokok = gajiPokok;
+    }
+    //setter gaji pokok
+    public void setGajiPokok(double gajiPokok){
+        if(gajiPokok < 0){
+            System.out.printf("sistem menolak nominal yang Anda masukkan kurang dari nol");
+        } else {
+            this.gajiPokok = gajiPokok;
+        }
+    }
+
+    public void setDepartemen(String departemen) {
+        this.departemen = departemen;
+    }
+
+    public void tampilkanDataKaryawan() {
+        System.out.println("\nProfil Lengkap Karyawan");
+        System.out.println("ID Karyawan  : " + this.idKaryawan);
+        System.out.println("Nama Karyawan: " + this.nama);
+        System.out.println("Departemen   : " + this.departemen);
+        System.out.println("Gaji Pokok   : " + this.gajiPokok);
+    }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -34,11 +65,22 @@ public class Karyawan {
         String nama = input.nextLine();
 
         Karyawan karyawan1 = new Karyawan(idKaryawan, nama);
-        System.out.println("\t");
 
+        //soal no. 2
+        double gajiPokok1;
+        do {
+            System.out.print("Masukkan Gaji Pokok Karyawan: ");
+            gajiPokok1 = input.nextDouble();
+            karyawan1.setGajiPokok(gajiPokok1);
+        } while (gajiPokok1 < 0);
+
+        //soal NO. 1
         System.out.println("Data Karyawan\t");
         System.out.println("ID Karyawan: " + karyawan1.getIdKaryawan());
         System.out.println("Nama Karyawan: " + karyawan1.getNama());
+        System.out.println("Gaji Pokok: " + karyawan1.getGajiPokok());
+        
+        karyawan1.tampilkanDataKaryawan();
 
         input.close();
     }
